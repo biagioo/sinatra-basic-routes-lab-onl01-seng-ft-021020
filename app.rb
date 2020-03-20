39591142629264991b148c1192fcef6c29f7ec2d
biagioo <mendolia2328@gmail.com>
2020-03-20 00:00:37 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Biago"
+    end
+
+    get '/hometown' do
+        "My hometown is Hauppauge"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is All girls are the same"
+    end
+
 end
